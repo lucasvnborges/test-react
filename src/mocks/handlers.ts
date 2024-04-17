eadf10ab2f5cbfb4caa4d4daafd4160e23007133
Lucas Borges <lucasvnborges@gmail.com>
2024-04-17 09:02:09 -0300

    feat: add manipuladores MSW para simulação de API

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,30 @@
-import { http } from 'msw'
+import { http, HttpResponse } from "msw";
+import { SessionStorageMap } from "./sessionStorage";
 
-export const handlers = []
+const storage = new SessionStorageMap<string, any>();
+
+export const handlers = [
+  http.get("/clientes", () => {
+    return HttpResponse.json(Array.from(storage.values()));
+  }),
+
+  http.post("/clientes", ({ request }) => {
+    const id = Date.now().toString();
+    const cliente = { ...request.body, id };
+    storage.set(id, cliente);
+    return HttpResponse.json(cliente);
+  }),
+
+  http.put("/clientes/:id", ({ params, request }) => {
+    const { id } = params;
+    const cliente = { ...request.body, id };
+    storage.set(id.toString(), cliente);
+    return HttpResponse.json(cliente);
+  }),
+
+  http.delete("/clientes/:id", ({ params }) => {
+    const { id } = params;
+    storage.delete(id.toString());
+    return new HttpResponse("Success", { status: 200 });
+  }),
+];
