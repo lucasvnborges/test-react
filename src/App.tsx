acff6b8b9937016aa1fa4d7aad04ef04b2d1d388
Lucas Borges <lucasvnborges@gmail.com>
2024-04-23 19:57:45 -0300

    feat: adicionar query client provider ao componente root

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 import { CustomerList, CreateCustomer } from './screens'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<CustomerList />} />
-        <Route path="novo-cliente" element={<CreateCustomer />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/">
+          <Route index element={<CustomerList />} />
+          <Route path="novo-cliente" element={<CreateCustomer />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   )
 }
