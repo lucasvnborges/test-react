7bf5a5adf63b63b07e65f7d2621bb80d1b1c62d5
Lucas Borges <lucasvnborges@gmail.com>
2024-04-23 13:13:38 -0300

    feat(CustomerSchema): adicionar validação as propriedades do schema

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,37 +1,44 @@
 import { z } from 'zod'
 
+const phoneRegex = /^\(?\d{2}\)?\s?9\d{4}-?\d{4}$/
+
 const IndividualSchema = z.object({
   type: z.literal('PF'),
-  name: z
-    .string()
-    .min(5, { message: 'Preencha com um nome completo válido' }),
-  cpf: z
-    .string()
-    .length(14, { message: 'Preencha com um número de CPF válido' }),
-  email: z.string().email('Preencha com um e-mail válido'),
-  phone: z.string().refine(
+  name: z.string().min(5, { message: 'Preencha com um nome completo válido' }),
+  cpf: z.string().refine(
     (value) => {
-      const phoneRegex = /^\(?\d{2}\)?\s?9\d{4}-?\d{4}$/
-      return phoneRegex.test(value)
+      const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+      return cpfRegex.test(value)
     },
     {
-      message: 'Preencha com um número de telefone válido',
+      message: 'Preencha com um CPF válido',
     }
   ),
+  email: z.string().email('Preencha com um e-mail válido'),
+  phone: z.string().refine((value) => phoneRegex.test(value), {
+    message: 'Preencha com um número de telefone válido',
+  }),
 })
 
 const CorporateSchema = z.object({
   type: z.literal('PJ'),
-  name: z.string(),
-  fantasy_name: z.string(),
-  cnpj: z.string().length(18),
-  email: z.string().email(),
-  phone: z
+  name: z.string().min(5, { message: 'Preencha com uma razão social válida' }),
+  fantasy_name: z
     .string()
-    .regex(/^\(?\d{2}\)?\s?9\d{4}-?\d{4}$/)
-    .refine((value) => /^\(?\d{2}\)?\s?9/.test(value), {
-      message: 'Preencha com um número de telefone válido',
-    }),
+    .min(5, { message: 'Preencha com um nome fantasia válido' }),
+  cnpj: z.string().refine(
+    (value) => {
+      const cnpjRegex = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/
+      return cnpjRegex.test(value)
+    },
+    {
+      message: 'Preencha com um CNPJ válido',
+    }
+  ),
+  email: z.string().email('Preencha com um e-mail válido'),
+  phone: z.string().refine((value) => phoneRegex.test(value), {
+    message: 'Preencha com um número de telefone válido',
+  }),
 })
 
 export const CustomerSchema = z.union([IndividualSchema, CorporateSchema])
