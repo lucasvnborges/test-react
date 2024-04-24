0ba38cc51d3cca6e7a1c5e405f9736534649d1ce
Lucas Borges <lucasvnborges@gmail.com>
2024-04-24 02:24:41 -0300

    fix: ajustar componente de menu para tela de edição de cliente

--- src/components/CustomerTypeMenu.tsx
+++ src/components/CustomerTypeMenu.tsx
@@ -3,6 +3,7 @@ import { Box, Grid, Typography } from '@mui/material'
 
 type ItemProps = {
   selected?: boolean
+  disabled?: boolean
 }
 
 type Props = {
@@ -12,7 +13,8 @@ type Props = {
     icon: any
   }[]
   selectedOption: any
-  onChange: (value: any) => void
+  onChange?: (value: any) => void
+  isEditing?: boolean
 }
 
 const MenuItem = styled(Grid)<ItemProps>`
@@ -22,18 +24,25 @@ const MenuItem = styled(Grid)<ItemProps>`
   justify-content: center;
   padding: 16px 24px;
   border-radius: 4px;
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
   border: solid 1px ${({ selected }) => (selected ? '#ccc' : '#ddd')};
   background-color: ${({ selected }) => (selected ? '#f1f1f1' : '#ffffff')};
 `
 
-export default function Menu({ options, selectedOption, onChange }: Props) {
+export default function Menu({
+  options,
+  selectedOption,
+  onChange,
+  isEditing,
+}: Props) {
   return (
     <Grid container spacing={3} mb={3}>
       {options.map((option) => (
         <Grid key={option.value} item xs={12} sm={6}>
           <MenuItem
-            onClick={() => onChange(option.value)}
             selected={option.value === selectedOption}
+            disabled={isEditing && option.value !== selectedOption}
+            onClick={() => !isEditing && onChange && onChange(option.value)}
           >
             <Box mr={1}>{option.icon}</Box>
             <Typography>{option.label}</Typography>
