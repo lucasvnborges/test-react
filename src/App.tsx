9c142934647e75dfc1d7ebd93e40589405661dba
Lucas Borges <lucasvnborges@gmail.com>
2024-04-24 02:28:55 -0300

    feat: adicionar rota de editar cliente

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { CustomerList, CreateCustomer } from './screens'
+import { CustomerList, CreateCustomer, EditCustomer } from './screens'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -10,7 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/">
           <Route index element={<CustomerList />} />
-          <Route path="novo-cliente" element={<CreateCustomer />} />
+          <Route path="cadastrar-cliente" element={<CreateCustomer />} />
+          <Route path="editar-cliente/:id" element={<EditCustomer />} />
         </Route>
       </Routes>
     </QueryClientProvider>
